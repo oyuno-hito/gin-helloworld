79a20afd6d5a753f8330f1ee7984a4e88d5fde80
oyuno-hito <yuta.oyunohito@gmail.com>
2024-02-14 19:00:25 +0900

    oapi-codegenを実行するshellスクリプトと自動生成されたコードを追加

---
+++ generate_from_openapi.sh
@@ -0,0 +1,2 @@
+oapi-codegen -generate "types" -package openapi schema/gin-helloworld.yml > ./src/openapi/types.gen.go
+oapi-codegen -generate "gin" -package openapi schema/gin-helloworld.yml > ./src/openapi/gin.gen.go
