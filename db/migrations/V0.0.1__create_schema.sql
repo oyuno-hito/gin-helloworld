b791111a312d5e2816843a46dc7a71964e15054c
oyuno-hito <yuta.oyunohito@gmail.com>
2024-02-15 14:43:07 +0900

    migrationファイル追加

---
+++ db/migrations/V0.0.1__create_schema.sql
@@ -0,0 +1,2 @@
+CREATE SCHEMA IF NOT EXISTS `gin-helloworld` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin ;
+USE `gin-helloworld` ;
---
+++ db/migrations/V0.1.1__insert_user_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO `gin-helloworld`.`users` (`login_id`,`password`,`name`, `role_id`)
+VALUES
+    ('user1', 'password', '管理者太郎', 1),
+    ('user2', 'password', '開発者太郎', 2),
+    ('user3', 'password', 'ユーザー太郎', 3);
