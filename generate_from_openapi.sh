4c2d70ee074892a695d100d67940c360b396f4c1
oyuno-hito <yuta.oyunohito@gmail.com>
2024-03-06 18:21:26 +0900

    middlewareで認証バリデーションをかけたいためopen apiの修正と導入に必要な修正対応を実施

--- generate_from_openapi.sh
+++ generate_from_openapi.sh
@@ -1,2 +1,3 @@
 oapi-codegen -generate "types" -package openapi schema/gin-helloworld.yml > ./src/openapi/types.gen.go
 oapi-codegen -generate "gin" -package openapi schema/gin-helloworld.yml > ./src/openapi/gin.gen.go
+oapi-codegen -generate "spec" -package openapi schema/gin-helloworld.yml > ./src/openapi/spec.gen.go
